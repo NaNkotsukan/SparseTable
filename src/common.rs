3c4943364c647475e7043b8727d29533caeb24c3
Met Martinez <nankotsukan@gmail.com>
2023-03-26 19:17:15 +0900

    get_msb_pos の warning を修正

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
 #[inline(always)]
 pub unsafe fn get_msb_pos(x: u64) -> u64 {
-    let mut ret = 0u64;
+    let mut ret: u64;
     std::arch::asm!(
         "lzcnt {ret:r}, {bits:r}",
         bits = in(reg) x,
