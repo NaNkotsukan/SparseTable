9744e3e4496cc6d5ff38bac0520fc201fe197507
Met Martinez <nankotsukan@gmail.com>
2023-11-05 22:20:51 +0900

    main.rs を修正

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,29 @@ mod rmq;
 use crate::rmq::RMQ;
 use rand::Rng;
 use rkyv::ser::{Serializer, serializers::AllocSerializer};
+mod compare;
 
-type T = u16;
+use compare::{CompareTrait, MinMaxTrait};
+
+type T = i16;
 
 fn main() {
+    impl CompareTrait for T {
+        fn cmp(a: &Self, b: &Self) -> std::cmp::Ordering {
+            a.cmp(b)
+        }
+    }
+    impl MinMaxTrait for T {}
     let num = 3000;
     let mut rng = rand::thread_rng();
-    let arr = (&mut rng).sample_iter(rand::distributions::Uniform::new(0, u16::MAX)).take(num).collect::<Vec<_>>();
-    let rmq = crate::rmq::RMQ::<u16>::new(&arr);
+    let arr = (&mut rng).sample_iter(rand::distributions::Uniform::new(0, T::MAX)).take(num).collect::<Vec<_>>();
+    let rmq = crate::rmq::RMQ::<T>::new(&arr);
 
     let mut serializer = AllocSerializer::<0>::default();
     serializer.serialize_value(&rmq).unwrap();
     let bytes = serializer.into_serializer().into_inner();
     
-    let archived = unsafe { rkyv::archived_root::<crate::rmq::RMQ::<u16>>(&bytes[..]) };
+    let archived = unsafe { rkyv::archived_root::<crate::rmq::RMQ::<T>>(&bytes[..]) };
         
     for i in 0..num {
         println!("{} / {}", i, num);
