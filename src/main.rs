06f955317fb980e7995a30d3521be6b1707eed1c
Met Martinez <nankotsukan@gmail.com>
2023-11-06 00:01:54 +0900

    main の修正

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,55 @@ mod rmq;
 use crate::rmq::RMQ;
 use rand::Rng;
 use rkyv::ser::{Serializer, serializers::AllocSerializer};
+use rkyv::{Archive, Deserialize, Serialize, Archived};
 mod compare;
 
 use compare::{CompareTrait, MinMaxTrait};
 
-type T = i16;
+#[derive(Archive, Serialize)]
+#[archive_attr(derive(Clone, Copy))]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Hoge {
+    a: std::num::NonZeroU16,
+}
+
+impl Default for Hoge {
+    fn default() -> Self {
+        Self {
+            a: unsafe { std::mem::transmute(0u16) }
+        }
+    }
+}
+
+type T = Hoge;
 
 fn main() {
     impl CompareTrait for T {
         fn cmp(a: &Self, b: &Self) -> std::cmp::Ordering {
-            a.cmp(b)
+            a.a.cmp(&b.a)
         }
     }
     impl MinMaxTrait for T {}
+    impl CompareTrait for Archived<T> {
+        fn cmp(a: &Self, b: &Self) -> std::cmp::Ordering {
+            a.a.cmp(&b.a)
+        }
+    }
+    impl MinMaxTrait for Archived<T> {}
+
     let num = 3000;
     let mut rng = rand::thread_rng();
-    let arr = (&mut rng).sample_iter(rand::distributions::Uniform::new(0, T::MAX)).take(num).collect::<Vec<_>>();
-    let rmq = crate::rmq::RMQ::<T>::new(&arr);
+    let arr = (&mut rng).sample_iter(rand::distributions::Uniform::new(1, u16::MAX))
+        .take(num)
+        .map(|x| Hoge{a: std::num::NonZeroU16::new(x).unwrap()})
+        .collect::<Vec<_>>();
+    let rmq: RMQ<Hoge> = crate::rmq::RMQ::<T>::new(&arr);
 
     let mut serializer = AllocSerializer::<0>::default();
     serializer.serialize_value(&rmq).unwrap();
     let bytes = serializer.into_serializer().into_inner();
     
-    let archived = unsafe { rkyv::archived_root::<crate::rmq::RMQ::<T>>(&bytes[..]) };
+    let archived: &rmq::ArchivedRMQ<Hoge> = unsafe { rkyv::archived_root::<crate::rmq::RMQ::<T>>(&bytes[..]) };
         
     for i in 0..num {
         println!("{} / {}", i, num);
